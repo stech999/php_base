ef771faabdcfa444bbe3e92b0ed3bb7420c1b72c
Красавчик <stech999@ya.ru>
2024-10-11 16:37:52 +0300

    less9

---
+++ lesson9_dz/app/index.php
@@ -0,0 +1,9 @@
+<?php
+
+try {
+    require_once(__DIR__ . '/vendor/autoload.php');
+    echo \App::app()->run();
+} catch (Throwable $e) {
+    // TODO после выноса рендера, вызывать его
+    var_dump($e);
+}
